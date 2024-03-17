dddc9d4d3510ff7e140b02d51b28ea3960be18cb
amitcarpenter <amitcarpenter199@gmail.com>
2024-03-17 14:54:45 +0530

    frontend part complete

--- backend/controllers/loaderController.js
+++ backend/controllers/loaderController.js
@@ -1,3 +1,14 @@
+const login_loader = async (req, res) => {
+  res.render("pages/samples/login.ejs");
+};
+
+const error_500_loader = async (req, res) => {
+  res.render("pages/samples/error-500.ejs");
+};
+const error_404_loader = async (req, res) => {
+  res.render("pages/samples/error-404.ejs");
+};
+
 const dashbaord_loader = async (req, res) => {
   res.render("index.ejs");
 };
@@ -34,6 +45,34 @@ const testimonial_create_page_loader = async (req, res) => {
   res.render("pages/testimonial/testimonial-create.ejs");
 };
 
+const featuredlist_page_loader = async (req, res) => {
+  res.render("pages/featuredlist/featuredlist.ejs");
+};
+
+const featuredlist_create_page_loader = async (req, res) => {
+  res.render("pages/featuredlist/featuredlist-create.ejs");
+};
+
+const custom_property_create_page_loader = async (req, res) => {
+  res.render("pages/featuredlist/custom-property-create.ejs");
+};
+
+const brokerage_page_loader = async (req, res) => {
+  res.render("pages/featuredlist/brokerage.ejs");
+};
+
+const brokerage_create_page_loader = async (req, res) => {
+  res.render("pages/featuredlist/brokerage-create.ejs");
+};
+
+const precondo_page_loader = async (req, res) => {
+  res.render("pages/precondo/precondo.ejs");
+};
+
+const precondo_create_page_loader = async (req, res) => {
+  res.render("pages/precondo/precondo-create.ejs");
+};
+
 module.exports = {
   dashbaord_loader,
   about_page_loader,
@@ -44,4 +83,14 @@ module.exports = {
   enquiry_page_loader,
   testimonial_page_loader,
   testimonial_create_page_loader,
+  login_loader,
+  error_500_loader,
+  error_404_loader,
+  featuredlist_page_loader,
+  featuredlist_create_page_loader,
+  custom_property_create_page_loader,
+  brokerage_page_loader,
+  brokerage_create_page_loader,
+  precondo_page_loader,
+  precondo_create_page_loader,
 };
--- backend/routes/loaderRoutes.js
+++ backend/routes/loaderRoutes.js
@@ -8,12 +8,28 @@ const {
   meta_data_page_loader,
   enquiry_page_loader,
   testimonial_page_loader,
-  testimonial_create_page_loader
+  testimonial_create_page_loader,
+  login_loader,
+  error_404_loader,
+  error_500_loader,
+  featuredlist_page_loader,
+  featuredlist_create_page_loader,
+  custom_property_create_page_loader,
+  brokerage_page_loader,
+  brokerage_create_page_loader,
+  precondo_page_loader,
+  precondo_create_page_loader,
 } = require("../controllers/loaderController");
 
 const loader_router = express.Router();
 
 loader_router.get("/", dashbaord_loader);
+
+loader_router.get("/login", login_loader);
+
+loader_router.get("/404", error_404_loader);
+
+loader_router.get("/500", error_500_loader);
 
 loader_router.get("/about", about_page_loader);
 
@@ -31,6 +47,21 @@ loader_router.get("/testimonial", testimonial_page_loader);
 
 loader_router.get("/testimonial-create", testimonial_create_page_loader);
 
+loader_router.get("/featuredlist", featuredlist_page_loader);
 
+loader_router.get("/featuredlist-create", featuredlist_create_page_loader);
+
+loader_router.get(
+  "/custom-property-create",
+  custom_property_create_page_loader
+);
+
+loader_router.get("/brokerage", brokerage_page_loader);
+
+loader_router.get("/brokerage-create", brokerage_create_page_loader);
+
+loader_router.get("/precondo", precondo_page_loader);
+
+loader_router.get("/precondo-create", precondo_create_page_loader);
 
 module.exports = loader_router;
